95cedd522d01a4786edc209ec0e06001c8fb1aa6
faulomi <jerome.m@grouplease.co.th>
2018-03-24 14:05:38 +0700

    [Axios] Complete REST API client implementation

--- src/services/api/EmployeeApplicationService.js
+++ src/services/api/EmployeeApplicationService.js
@@ -11,4 +11,43 @@ export default class EmployeeApplicationService {
         });
   }
 
+  static create(application) {
+    return axios.post(endpoint, application);
+  }
+
+  static updatePersonalInformation(applicationId, personalInformation) {
+
+    return axios.put(endpoint + "/" + applicationId + "/personal-info");
+  }
+
+  static updateEducation(applicationId, education) {
+
+    return axios.put(endpoint + "/" + applicationId + "/education");
+  }
+
+  static updateWorkingExperience(applicationId, workingExperience) {
+
+    return axios.put(endpoint + "/" + applicationId + "/working-experience");
+  }
+
+  static getPersonalInformation(applicationId, personalInformation) {
+
+    return axios.get(endpoint + "/" + applicationId + "/personal-info");
+  }
+
+  static getEducation(applicationId, education) {
+
+    return axios.get(endpoint + "/" + applicationId + "/education");
+  }
+
+  static getWorkingExperience(applicationId, workingExperience) {
+
+    return axios.get(endpoint + "/" + applicationId + "/working-experience");
+  }
+
+  static get(applicationId) {
+
+    return axios.get(endpoint + "/" + applicationId).then(
+        response => Promise.resolve(response.data));
+  }
 }
